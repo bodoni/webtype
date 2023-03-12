ad774c66104fa85b98418cd88ebcaccbbdf99288
Ivan Ukhov <ivan.ukhov@gmail.com>
2023-03-12 11:45:38 +0100

    Add submodules for the two versions

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
 //! Parser for fonts in Web Open Font Format (WOFF).
+
+pub mod version1;
+pub mod version2;
---
+++ src/version2.rs
@@ -0,0 +1,3 @@
+//! The [Web Open Font Format][1] of version 2.0.
+//!
+//! [1]: https://www.w3.org/TR/WOFF2/
